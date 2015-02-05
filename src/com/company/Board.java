01c2f9358284efeb6a1f2533aaed2ea09bf165de
LeonStel <leonvanderstel@hotmail.com>
2015-02-05 20:14:12 +0100

    update

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,35 +1,30 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Board {
 
     int[][] board;
-    private final ArrayList<Vector> availableBoardCo = new ArrayList<Vector>();
+    private final HashSet<Vector> availableBoardCo = new HashSet<Vector>();
 
     Piece[] pieces;
 
     private ArrayList<Piece> currentPieces = new ArrayList<Piece>();
 
     public Board(){
+
         board = new int[][] {
                 {0, 1, 2, 3, 4},
                 {0, 1, 2, 3, 4, 5},
                 {0, 1, 2, 3, 4, 5, 6},
                 {0, 1, 2, 3, 4, 5, 6, 7},
                 {0, 1, 2, 3, 4, 5, 6, 7, 8},
-                {0, 1, 2, 3, 4, 5, 6, 7},
-                {0, 1, 2, 3, 4, 5, 6},
-                {0, 1, 2, 3, 4, 5},
-                {0, 1, 2, 3, 4}
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {2, 3, 4, 5, 6, 7, 8},
+                {3, 4, 5, 6, 7, 8},
+                {4, 5, 6, 7, 8}
         };
-
-//        board = new int[][] {
-//                {0, 1, 2, 3, 4},
-//                {0, 1, 2, 3, 4, 5},
-//                {0, 1, 2, 3, 4, 5, 6},
-//                {0, 1, 2, 3, 4, 5, 6, 7}
-//        };
 
         availableBoardCo.add(new Vector(0,0));
         availableBoardCo.add(new Vector(1,0));
@@ -65,7 +60,7 @@ public class Board {
         availableBoardCo.add(new Vector(1,4));
         availableBoardCo.add(new Vector(2,4));
         availableBoardCo.add(new Vector(3,4));
-        availableBoardCo.add(new Vector(4,4));
+//        availableBoardCo.add(new Vector(4,4));
         availableBoardCo.add(new Vector(5,4));
         availableBoardCo.add(new Vector(6,4));
         availableBoardCo.add(new Vector(7,4));
@@ -116,19 +111,66 @@ public class Board {
                 new Piece(12)
         };
 
-        SolveEnigma(1);
+        if (SolveEnigma(1)) {
+            System.out.println("!! Solved !!");
+        }
+        else {
+            System.out.println("?? Not solved ??");
+        }
     }
 
+    private long startTime;
+    private int count1,count2;
+
+    static Vector[] checkOrphan  = { new Vector (-1, 0),
+            new Vector ( 1, 0),
+            new Vector ( 0,-1),
+            new Vector ( 0, 1),
+            new Vector (-1, 1),
+            new Vector ( 1,-1), };
+
+    private boolean orphanCheck (Vector currentBoardCo) {
+        for (int i=0; i<checkOrphan.length; i++) {
+            if (availableBoardCo.contains(currentBoardCo.add(checkOrphan[i]))) {
+                return false;
+            }
+        }
+        return true;
+    }
     private boolean SolveEnigma(int p) {
+
+        if (p==1) {
+            startTime = System.currentTimeMillis();
+            count1=1;
+        }
 
         Vector currentBoardCo = new Vector(0,0);
 
         Piece piece = pieces[p-1];
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[y].length; x++) {
-                currentBoardCo = new Vector(x, y);
+                currentBoardCo.Set(board[y][x], y);
 
 //                System.out.println("lay down p : "+p+" at ["+x+","+y+"]");
+
+                if (p == 1) {
+                    System.out.println();
+                    System.out.print("[" + count1 + "/" + ((System.currentTimeMillis()- startTime)/1000) + "]");
+                    count1++;
+                    count2=1;
+                }
+                if (p == 2) {
+                    System.out.print("#"+count2);
+                    count2++;
+                }
+
+                if (! availableBoardCo.contains(currentBoardCo)) {
+                    continue;
+                }
+
+                if (orphanCheck(currentBoardCo)) {
+                    return false;
+                }
 
                 for(int v = 1; v<4; v++) {
                     boolean result = TryToFit(piece, v, currentBoardCo);
@@ -143,12 +185,13 @@ public class Board {
                                 currentPieces.remove(currentPieces.size() - 1);
                             }
                         } else {
-//                            System.out.println("finish");
+                            System.out.println();
+                            System.out.println("finish");
                             for (int c = 0; c<currentPieces.size(); c++){
                                 System.out.println(
                                         "piece : "+currentPieces.get(c).piece+
-                                        " version 1 : "+currentPieces.get(c).currentVersion+
-                                        " currentBoardCo : ["+currentPieces.get(c).currentBoardCo.x+","+currentPieces.get(c).currentBoardCo.y+"]");
+                                                " version 1 : "+currentPieces.get(c).currentVersion+
+                                                " currentBoardCo : ["+currentPieces.get(c).currentBoardCo.x+","+currentPieces.get(c).currentBoardCo.y+"]");
                             }
                             return true;
                         }
@@ -160,44 +203,43 @@ public class Board {
     }
 
     public boolean TryToFit(Piece piece, int v, Vector currentBoardCo){
-            boolean pieceFit = true;
+        Vector[] version = getVersionCoos(piece, v);
 
-            Vector[] version = getVersionCoos(piece, v);
+        for (int co = 0; co < version.length; co++) {
+            Vector versionCo = currentBoardCo.add(version[co]);
 
-            for (int co = 0; co < version.length; co++) {
-                Vector versionCo = currentBoardCo.add(version[co]);
-
-                boolean match = false;
-
-                for (int empty = 0; empty < availableBoardCo.size(); empty++) {
-                    Vector emptySpot = availableBoardCo.get(empty);
-                    if (versionCo.equals(emptySpot)) {
-                        match = true;
-                        break;
-                    } else {
-                        match = false;
-                    }
-                }
-
-                if (!match) {
-                    pieceFit = false;
-                }
-
-                if (co == version.length - 1 && pieceFit == true) {
-                    //lay piece down, discard all used empty places
-                    piece.currentVersion = v;
-                    piece.currentBoardCo = currentBoardCo;
-                    currentPieces.add(piece);
-                    discardEmptySpots(currentBoardCo, version);
-                    return true;
-                }
+            if (! availableBoardCo.contains(versionCo)) {
+                return false;
             }
-            return false;
+/*
+          boolean bMatch = false;
+          for (int empty = 0; empty < availableBoardCo.size(); empty++) {
+              Vector emptySpot = availableBoardCo.get(empty);
+
+              if ( versionCo.equals(emptySpot)) {
+                 bMatch = true;
+                 break;
+              }
+          }
+          if (! bMatch) {
+             return false;
+          }
+  */
+        }
+
+        //lay piece down, discard all used empty places
+        piece.currentVersion = v;
+        piece.currentBoardCo = currentBoardCo;
+        currentPieces.add(piece);
+        discardEmptySpots(currentBoardCo, version);
+        return true;
     }
 
     public void discardEmptySpots(Vector currentBoardCo, Vector[] version){
         for (int co = 0; co < version.length; co++) {
             Vector versionCo = currentBoardCo.add(version[co]);
+            availableBoardCo.remove (versionCo);
+/*
             for (int empty = 0; empty < availableBoardCo.size(); empty++) {
                 Vector emptySpot = availableBoardCo.get(empty);
                 if(versionCo.equals(emptySpot)){
@@ -205,6 +247,7 @@ public class Board {
                     break;
                 }
             }
+*/
         }
     }
 
@@ -218,19 +261,14 @@ public class Board {
     }
 
     public Vector[] getVersionCoos(Piece piece, int versiontInt){
-        Vector[] version = new Vector[0];
-
         switch (versiontInt){
+            default:
             case 1:
-                version = piece.version1;
-                break;
+                return piece.version1;
             case 2:
-                version = piece.version2;
-                break;
+                return piece.version2;
             case 3:
-                version = piece.version3;
-                break;
+                return piece.version3;
         }
-        return version;
     }
 }
--- src/com/company/Vector.java
+++ src/com/company/Vector.java
@@ -10,11 +10,34 @@ public class Vector {
         this.y = y;
     }
 
+    public void Set (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     public Vector add(Vector v){
         return new Vector(this.x + v.x, this.y + v.y);
     }
 
     public boolean equals(Vector v){
         return this.x == v.x && this.y == v.y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Vector))
+            return false;
+        if (obj == this)
+            return true;
+
+        return this.x == ((Vector)obj).x && this.y == ((Vector)obj).y;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.x;
+        hash = 47 * hash + this.y;
+        return hash;
     }
 }
